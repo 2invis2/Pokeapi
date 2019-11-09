742504dc6fd1a4e13f6b2692cdb9ab194ffe99ee
Oleg <invisible6462@gmail.com>
2019-11-09 21:00:59 +0700

    Created Entity
    Created entity Pokemon
    Connection Lombok

---
+++ app/src/main/java/com/invis/pokeapi/feature/entity/Pokemon.java
@@ -0,0 +1,29 @@
+package com.invis.pokeapi.feature.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Pokemon {
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    private class Stat {
+        private String name;
+        private int value;
+    }
+
+    private String name;
+    private int number;
+    private int height;
+    private int weight;
+    private String type1;
+    private String type2;
+    private Stat hp;
+    private Stat attack;
+    private Stat defense;
+}
